f0d81dff5060289d9da788cc111e6261c5095478
nirmal10110 <nirmal.lather10110@gmail.com>
2020-06-22 17:40:40 +0530

    .

--- app.py
+++ app.py
@@ -12,17 +12,13 @@ app.config.from_object("default_config")
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 
 api.add_resource(Wallet, "/wallet/<string:mobile_number>")
 api.add_resource(WalletAmount, "/wallet/getamount")
 api.add_resource(AddWallet, "/addwallet")
 
 if __name__ == "__main__":
-    db.init_app(app)
+
     ma.init_app(app)
     app.run(port=5000, debug=True)
 
