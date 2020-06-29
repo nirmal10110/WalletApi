ba762d9bafaaa9bbc048bebcbef1b59deb5ad6af
nirmal10110 <nirmal.lather10110@gmail.com>
2020-06-29 13:20:00 +0530

    Shifted to Postgress

--- default_config.py
+++ default_config.py
@@ -1,5 +1,7 @@
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
+SQLALCHEMY_DATABASE_URI = "postgres://cdlvssvkcndjxs" \
+                                        ":063d2145766954c50cc0ec5a2d915903ec37a2a8d8c3a553dff53c75211c8208@ec2-34-194" \
+                                        "-198-176.compute-1.amazonaws.com:5432"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PROPAGATE_EXCEPTIONS = True
 SECRET_KEY = "change-this-key-in-the-application-config"
