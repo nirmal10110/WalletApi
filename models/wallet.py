d4cc0ff962a8b81744cf6a30596cdcaa7df3cf97
nirmal10110 <48415104+nirmal10110@users.noreply.github.com>
2020-06-30 22:50:48 +0530

    Update wallet.py

--- models/wallet.py
+++ models/wallet.py
@@ -12,11 +12,11 @@ class WalletModel(db.Model):
     def find_by_mobile_number(cls, mobile_no: str) -> "WalletModel":
         return cls.query.filter_by(mobile_number=mobile_no).first()
 
-    def reduce_amount(self, amount: int):
-        self.amount = (int(self.amount) - amount)
+    def reduce_amount(self, amount: float):
+        self.amount = (float(self.amount) - amount)
 
-    def add_amount(self, amount:int):
-        self.amount = (int(self.amount) + amount)
+    def add_amount(self, amount:float):
+        self.amount = (float(self.amount) + amount)
 
     def save_to_db(self):
         db.session.add(self)
